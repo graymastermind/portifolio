8e77f6bdd084aec693844a3491c0c10caad3ad66
graymastermind <82908514+graymastermind@users.noreply.github.com>
2023-06-09 22:43:00 +0200

    first

---
+++ 152i - Ben Mods & Skins for Minecraft/src/appresize.js
@@ -0,0 +1,224 @@
+//- Resize	
+
+const AppResize = (e) =>{
+	appObj.mainWidth		= Math.ceil(window.innerWidth);
+	appObj.mainHeight		= Math.ceil(window.innerHeight); 
+	appObj.canvasWidth		= Math.ceil(1.4*window.innerWidth);
+	appObj.canvasHeight		= Math.ceil(1.4*window.innerHeight); 
+	
+	renderer.view.style.width	= appObj.mainWidth+"px";
+	renderer.view.style.height	= appObj.mainHeight+"px";							
+	renderer.view.width			= appObj.canvasWidth;
+	renderer.view.height		= appObj.canvasHeight;
+	
+	renderer.resize(appObj.canvasWidth, appObj.canvasHeight);
+	
+	stage.position.set(Math.ceil(appObj.canvasWidth*0.5), Math.ceil(appObj.canvasHeight*0.5));
+					
+			
+	//- POSITION OBJ
+	
+	appMc.mcGame.scale.set(1, 1);	
+	appMc.mcUI.scale.set(1, 1);	
+	
+	if(appObj.mainWidth<appObj.mainHeight){	
+		appMc.mcGame.scale.x = appObj.canvasWidth/1280;
+		appMc.mcGame.scale.y = appMc.mcGame.scale.x;
+		if(appMc.mcGame.scale.y*1280 < appObj.canvasHeight){
+			appMc.mcGame.scale.y = appObj.canvasHeight/1280;
+			appMc.mcGame.scale.x = appMc.mcGame.scale.y;
+		}
+		
+		appMc.mcUI.scale.x = appObj.canvasWidth/720;
+		appMc.mcUI.scale.y = appMc.mcUI.scale.x;
+		if(appMc.mcUI.scale.y*1280 > appObj.canvasHeight){
+			appMc.mcUI.scale.y = appObj.canvasHeight/1280;
+			appMc.mcUI.scale.x = appMc.mcUI.scale.y;
+		}
+		
+							
+		appMc.mcBgFS.scale.x = 0.1 + appObj.canvasWidth/1280/appMc.mcUI.scale.x;
+		appMc.mcBgFS.scale.y = 0.1 + appObj.canvasHeight/1280/appMc.mcUI.scale.x;
+		
+		appMc.mcBgOverlay.scale.x = 0.1 + appObj.canvasWidth/1280/appMc.mcUI.scale.x;
+		appMc.mcBgOverlay.scale.y = 0.1 + appObj.canvasHeight/1280/appMc.mcUI.scale.x;
+
+		TweenLite.killTweensOf(appMc.startBattle);
+		appMc.startBattle.scale.set(0.5);
+		gsap.from(appMc.startBattle.scale, 0.2,{delay:0.2, overwrite:"none", x:0, y:0, ease:Sine.easeOut});
+		gsap.to(appMc.startBattle.scale, 0.5,{delay:0.4, yoyo:true, repeat:-1, overwrite:"none", x:"-=0.05", y:"-=0.05", ease:Sine.easeInOut});
+	
+
+		appMc.CharacterBoardBar.scale.x = 0.1 + appObj.canvasWidth/1280/appMc.mcUI.scale.x;
+		appMc.CharacterBoardBar.scale.y = 0.1 + appObj.canvasHeight/1280/appMc.mcUI.scale.x;
+
+		appMc.characters.scale.x = 0.1 + appObj.canvasWidth/1280/appMc.mcUI.scale.x;
+		appMc.characters.scale.y = 0.1 + appObj.canvasHeight/1280/appMc.mcUI.scale.x;
+
+		appMc.CharacterBoardContainer.y = -300;
+		appMc.CharacterBoardContainer.height = 350;
+
+
+		for(let i = 0; i<10; i++){
+			
+			appMc["level1CharacterIcon_"+i].scale.set(1)
+			appMc["level2character_"+i].scale.set(0.25)
+
+			appMc["name_"+i].scale.set(0.5)
+			appMc["name_"+i].y = -330
+			appMc["name_"+i].x = 0
+		}
+		
+		appMc.mcBtnSound.x = -60+appObj.canvasWidth*0.5/appMc.mcUI.scale.y;
+		appMc.mcBtnSound.y = -70+appObj.canvasHeight*0.5/appMc.mcUI.scale.y;
+		appMc.mcBtnSound.scale.set(0.65);
+
+		appMc.characters.y = appMc.CharacterBoardContainer.y+appMc.CharacterBoardContainer.height/2-appMc.caption.height/3;
+		appMc.characters.scale.set(0.12)
+
+		appMc.characters2.x = 110;
+
+		appMc.tapToSelect.y = appMc.CharacterBoardContainer.y+1.57*appMc.CharacterBoardContainer.height;
+		appMc.tapToSelect.scale.set(0.7)
+
+		appMc.armour.x = 0;
+		appMc.armour.y = -120+appObj.canvasHeight*0.5/appMc.mcUI.scale.y;
+		appMc.armour.scale.set(0.5)
+
+		appMc.attributes.scale.set(0.6)
+		appMc.attributes.x = 200-appObj.canvasWidth*0.5/appMc.mcUI.scale.y;
+		appMc.attributes.y = 0;
+
+		appMc.startBattle.x = 0;
+		appMc.startBattle.y = 330;
+
+		appMc.bush.y = -670+appObj.canvasHeight*0.5/appMc.mcUI.scale.y;
+		appMc.bush.scale.set(0.5, 0.55);
+
+		appMc.bush2.x = 110
+		appMc.bush2.scale.set(0.45)
+
+		appMc.vfxBush.y = 400
+		appMc.vfxBush.x = 110
+
+		appMc.telescope.scale.set(0.3, 0.25);
+		appMc.telescope.x=350
+		appMc.telescope.y=-150+appObj.canvasHeight*0.5/appMc.mcUI.scale.y;
+
+		appMc.shadow.y=-217+appObj.canvasHeight*0.5/appMc.mcUI.scale.y;
+		appMc.shadow.x=40
+		appMc.shadow.scale.set(0.15, 0.13);
+
+		appMc.arrow_l.y=-213+appObj.canvasHeight*0.5/appMc.mcUI.scale.y;
+		appMc.arrow_r.y=-213+appObj.canvasHeight*0.5/appMc.mcUI.scale.y;
+
+		appMc.arrow_l.x=-60+appObj.canvasWidth*0.5/appMc.mcUI.scale.y;
+    	appMc.arrow_r.x=60-appObj.canvasWidth*0.5/appMc.mcUI.scale.y;
+
+		appMc.arrow_r.scale.set(0.15)
+		appMc.arrow_l.scale.set(0.15)
+
+		if(stateGame == 1){
+			appMc.vfxBush.visible = true;
+		}			
+
+	}else{
+		
+		appMc.mcGame.scale.x = appObj.canvasWidth/1280;
+		appMc.mcGame.scale.y = appMc.mcGame.scale.x;
+		if(appMc.mcGame.scale.y*1280 < appObj.canvasHeight){
+			appMc.mcGame.scale.y = appObj.canvasHeight/1280;
+			appMc.mcGame.scale.x = appMc.mcGame.scale.y;
+		}
+		
+		appMc.mcUI.scale.x = appObj.canvasWidth/1280;
+		appMc.mcUI.scale.y = appMc.mcUI.scale.x;	
+		if(appMc.mcUI.scale.y*720 > appObj.canvasHeight){
+			appMc.mcUI.scale.y = appObj.canvasHeight/720;
+			appMc.mcUI.scale.x = appMc.mcUI.scale.y;
+		}
+												
+		appMc.mcBgFS.scale.x = 0.1 + appObj.canvasWidth/1280/appMc.mcUI.scale.x;
+		appMc.mcBgFS.scale.y = 0.1 + appObj.canvasHeight/1280/appMc.mcUI.scale.x;
+		
+		appMc.mcBgOverlay.scale.x = 0.1 + appObj.canvasWidth/1280/appMc.mcUI.scale.x;
+		appMc.mcBgOverlay.scale.y = 0.1 + appObj.canvasHeight/1280/appMc.mcUI.scale.x;
+
+		appMc.CharacterBoardBar.scale.x = 0.1 + appObj.canvasWidth/1280/appMc.mcUI.scale.x;
+		appMc.CharacterBoardBar.scale.y = 1
+
+		appMc.characters.scale.x = 0.1 + appObj.canvasWidth/1280/appMc.mcUI.scale.x;
+		appMc.characters.scale.y = 0.1 + appObj.canvasHeight/1280/appMc.mcUI.scale.x;
+
+		appMc.CharacterBoardContainer.y = -215;		
+		appMc.CharacterBoardContainer.height = 250;
+
+		for(let i = 0; i<10; i++){
+			
+			appMc["level1CharacterIcon_"+i].scale.set(1)
+			appMc["level2character_"+i].scale.set(0.18)
+
+			appMc["name_"+i].scale.set(0.5)
+			appMc["name_"+i].y = 100-appObj.canvasHeight*0.5/appMc.mcUI.scale.y;
+		}
+		
+		appMc.mcBtnSound.x = -60+appObj.canvasWidth*0.5/appMc.mcUI.scale.y;
+		appMc.mcBtnSound.y = -30+appObj.canvasHeight*0.5/appMc.mcUI.scale.y;
+		appMc.mcBtnSound.scale.set(0.65);
+		
+		appMc.selectAvatar.y = 0.4*((appMc.CharacterBoardContainer.y - appMc.CharacterBoardContainer.height/2)-appObj.canvasHeight*0.5/appMc.mcUI.scale.y);
+
+		TweenLite.killTweensOf(appMc.startBattle);
+		appMc.startBattle.scale.set(0.3)
+		gsap.from(appMc.startBattle.scale, 0.2,{delay:0.2, overwrite:"none", x:0, y:0, ease:Sine.easeOut});
+		gsap.to(appMc.startBattle.scale, 0.5,{delay:0.4, yoyo:true, repeat:-1, overwrite:"none", x:"-=0.05", y:"-=0.05", ease:Sine.easeInOut});
+
+		appMc.characters.y = appMc.CharacterBoardContainer.y+appMc.CharacterBoardContainer.height/2-appMc.caption.height/3;
+		appMc.characters.scale.set(0.1)
+		
+		appMc.characters2.y = -15
+		appMc.characters2.x = 0
+
+		appMc.armour.scale.set(0.4)
+
+		appMc.attributes.scale.set(0.5)
+		appMc.attributes.x = -280+appObj.canvasWidth*0.5/appMc.mcUI.scale.y;
+		appMc.attributes.y = -60; 
+
+		appMc.tapToSelect.y = appMc.CharacterBoardContainer.y+1.25*appMc.CharacterBoardContainer.height;
+		appMc.tapToSelect.scale.set(0.45)
+
+		appMc.startBattle.x = 0;
+		appMc.startBattle.y = -183+appObj.canvasHeight*0.5/appMc.mcUI.scale.y;
+		appMc.startBattle.scale.set(0.3)
+
+		appMc.armour.x = 0;
+		appMc.armour.y = -80+appObj.canvasHeight*0.5/appMc.mcUI.scale.y;
+
+
+		appMc.bush.scale.set(0.3);
+		appMc.bush.y = -340+appObj.canvasHeight*0.5/appMc.mcUI.scale.y;
+
+		appMc.telescope.scale.set(1);
+		appMc.telescope.x=400
+		appMc.telescope.scale.set(0.2, 0.2)
+		appMc.telescope.y=-50+appObj.canvasHeight*0.5/appMc.mcUI.scale.y;
+
+		appMc.shadow.y=-130+appObj.canvasHeight*0.5/appMc.mcUI.scale.y;
+		appMc.shadow.x=90
+		appMc.shadow.scale.set(0.15);
+
+		appMc.arrow_l.y=-130+appObj.canvasHeight*0.5/appMc.mcUI.scale.y;
+		appMc.arrow_r.y=-130+appObj.canvasHeight*0.5/appMc.mcUI.scale.y;
+
+		appMc.arrow_l.x=300
+		appMc.arrow_r.x=-300
+
+		appMc.vfxBush.visible = false;
+
+		appMc.arrow_r.scale.set(0.13)
+		appMc.arrow_l.scale.set(0.13)
+
+	}
+}			
+// Resize	
---
+++ 152i - Ben Mods & Skins for Minecraft/src/initbasicobj.js
@@ -0,0 +1,207 @@
+const InitBasicObj = () =>{
+
+//- mcMain
+
+appMc.mcMain = new createContainer({p:stage, visible:false});					
+	appMc.mcGame = new createContainer({p:appMc.mcMain});					
+	
+	appMc.mcGame.twistFilter = new PIXI.filters.TwistFilter({
+		angle:4,
+		offset:{x:640,y:640},						
+		radius:800,
+		padding:20
+		});
+		 
+		appMc.mcWorldShake = new createContainer({p:appMc.mcGame});						
+		appMc.mcWorldShake.shakeAX = 0;
+		appMc.mcWorldShake.shakeAY = 0;
+		appMc.mcWorldShake.shakeD = 0;
+
+		if(physics){
+			appMc.p2world = new p2.World({ gravity: [0, -500]});
+
+			// Add a plane
+	        planeShape = new p2.Plane();
+	        planeBody = new p2.Body({ position:[0,-100] });
+	        planeBody.addShape(planeShape);
+	        appMc.p2world.addBody(planeBody);
+		}
+
+              
+		
+		//Back
+			appMc.mcWorldCamera = new createContainer({p:appMc.mcWorldShake});	
+				appMc.mcBg = new createSprite({p:appMc.mcWorldCamera,tex:"bg_0"});
+		
+			
+
+		//UI							
+			appMc.mcUI = new createContainer({p:appMc.mcMain});	
+
+			// Bush
+				appMc.bush = new createSprite({p:appMc.mcUI,tex:"bush", scale:0.35, visible:true});
+
+			// Bush
+				appMc.bush2 = new createSprite({p:appMc.mcWorldCamera,tex:"bush2", scale:0.35, visible:false});
+
+			// vfx Bush 2
+			appMc.vfxBush = new createSequence({
+				p:appMc.mcUI,
+				tex:"vfx",
+				data:vfxSpritesheet,
+				prefix:"vfx_",
+				frame:0,
+				totalFrame:6,
+				seq:0,
+				slow:0,
+				loop:false,
+				reverse:false,	
+					stopFrame:1,
+				alpha:0,
+				visible:true,
+				scale: 0.5
+				});
+
+			appMc.characters2 = new createContainer({p:appMc.mcUI, alpha:1});
+
+			// Telescope
+				appMc.telescope = new createSprite({p:appMc.mcUI,tex:"telescope", scale:0.35, visible:true});
+			
+			// Select Avatar Text
+				appMc.selectAvatar = new createSprite({p:appMc.mcUI,tex:"select_avatar", scale:0.5, visible:false});
+
+					// Character Bar container
+			appMc.CharacterBoardContainer = new createContainer({p:appMc.mcUI, alpha:1});
+
+			// Shadows container
+			appMc.shadow = new createContainer({p:appMc.mcUI, alpha:1, visible:false});
+			appMc.shadows = new createSprite({p:appMc.shadow ,tex:"shadow_"+selectedAvatar, visible:true});
+
+			// Names container
+			appMc.name = new createContainer({p:appMc.mcUI, alpha:1});
+
+			// Attributes
+			appMc.attributes = new createContainer({p:appMc.mcUI, alpha:1});
+			
+			// Character Bar container
+			appMc.abilities = new createContainer({p:appMc.mcUI, alpha:1});
+
+			// Armour
+			appMc.armour = new createContainer({p:appMc.mcUI, alpha:1});
+
+			// Armour
+			appMc.armour = new createContainer({p:appMc.mcUI, alpha:1});
+
+				//Character Bar container
+				appMc.CharacterBoardBar = new createRect({
+					p:appMc.CharacterBoardContainer,
+					x:-1280*0.5,
+					y:-1280*0,
+					width:1280,
+					height:300,
+					color:0x010101, 
+					lineWeigth: 10,
+					lineColor: 0xffffff,
+					fill:1,
+					alpha:0.9,
+					visible:true
+					});	
+
+				appMc.characters = new createContainer({p:appMc.mcUI, alpha:1});
+
+				//Set Filter
+				appMc.mcUI.colorFilter = new PIXI.filters.ColorMatrixFilter();	
+				appMc.mcUI.colorFilter.brightness(0.45);
+
+				//Set Filter
+				appMc.characters.colorFilter = new PIXI.filters.ColorMatrixFilter();	
+				appMc.characters.colorFilter.brightness(1);
+
+
+				for(let i = 0; i<10; i++){
+					appMc["character"+i] = new createContainer({p:appMc.characters});
+					appMc["level2character_"+i] = new createContainer({p:appMc.characters2});
+					appMc["level1CharacterIcon_"+i] = new createSprite({p:appMc["character"+i],tex:"avatar_"+i, x:-250+250*i*5, y:0});
+					appMc["level2CharacterIcon_"+i] = new createSprite({p:appMc["level2character_"+i],tex:"character_"+i, y:0, visible:false});
+					appMc["name_"+i] = new createSprite({p:appMc.name ,tex:"name_"+i, visible:false});
+					appMc["attribute_"+i] = new createSprite({p:appMc.attributes,tex:"attribute_"+i, visible:false});
+					appMc["ability_bar_"+i] = new createSprite({p:appMc.abilities,tex:"ability_bar_"+i, visible:false});
+					appMc["armour_"+i] = new createSprite({p:appMc.armour,tex:"armour_"+i, visible:false});
+					appMc["level1CharacterIcon_"+i].filters = [appMc.mcUI.colorFilter];
+				}	
+
+				appMc.caption = new createSprite({p:appMc.mcUI,tex:"caption_"+selectedAvatar, visible:false});
+
+			// Buttons
+			appMc.buttons = new createContainer({p:appMc.mcUI});
+			appMc.buttons.interactive = true;
+
+				// Select Avatar Text
+				appMc.tapToSelect = new createSprite({p:appMc.buttons,tex:"tap_to_select", scale:0.5});
+
+				// Select Avatar Text
+				appMc.startBattle = new createSprite({p:appMc.buttons,tex:"start_battle", scale:0.5, visible:false});
+
+			
+
+			// Arrows
+			appMc.arrows = new createContainer({p:appMc.mcUI, visible:false});
+
+			// Arrow left
+			appMc.arrow_r = new createSprite({p:appMc.arrows,tex:"arrow", scale:0.25, visible:true, id:1});
+
+			// Arrow right
+			appMc.arrow_l = new createSprite({p:appMc.arrows,tex:"arrow", scale:0.25, visible:true, id:2});
+			appMc.arrow_l.rotation = 3.14159
+
+			appMc.arrow_r.interactive = true;
+			appMc.arrow_l.interactive = true;
+
+				
+
+				
+			//- mcBgOverlay						
+				appMc.mcBgOverlay = new createRect({
+					p:appMc.mcUI,
+					x:-1280*0.5,
+					y:-1280*0.5, 
+					width:1280,
+					height:1280,
+					color:0x000000, 
+					alpha:0.0,
+					fill:1,
+					radius:0
+				});	
+			
+			
+			//- mcBgFS					
+			appMc.mcBgFS = new createRect({
+				p:appMc.mcUI,
+				x:-1280*0.5,
+				y:-1280*0.5, 
+				width:1280,
+				height:1280,
+				color:0x121214, 
+				alpha:0,
+				visible:false
+			});	
+
+appMc.mcBgOverlay.interactive = false;		
+appMc.mcBgOverlay.on('pointerdown', StageDown);	
+appMc.mcBgOverlay.on('pointermove', StageMove);
+appMc.mcBgOverlay.on('pointerup', StageUp);			
+appMc.mcBgFS.interactive = true;	
+appMc.mcBgFS.on('pointerup', ClickAd);
+
+//- mcBtnSound			
+appMc.mcBtnSound  = new createContainer({p:appMc.mcUI});			
+	appMc.mcBtnSoundB = new createSprite({p:appMc.mcBtnSound,tex:"btn_sound_off"});
+
+appMc.mcBtnSound.interactive = true;				
+appMc.mcBtnSound.on('pointerup', BtnGlobalSound);
+
+appMc.arrow_l.on("pointerdown", ArrowL);
+appMc.arrow_r.on("pointerdown", ArrowR);
+appMc.buttons.on("pointerdown", Buttons);
+
+}
